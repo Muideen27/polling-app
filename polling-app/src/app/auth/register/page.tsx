60e9b4ef1b5872e35c369398367de7133dfe5971
Muideen27 <ilorimuideen0000@gmail.com>
2025-08-26 19:11:04 +0100

    feat(auth): add login and register pages

---
+++ polling-app/src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from '@/components/RegisterForm'
+
+
+export default function RegisterPage() {
+return (
+<main className="min-h-[70vh] grid place-items-center p-4">
+<RegisterForm />
+</main>
+)
+}
